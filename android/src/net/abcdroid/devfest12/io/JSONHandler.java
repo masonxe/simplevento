155fe8b96eaf3040a59176a155f1ef62297494ed
dmotta <mdavid20@gmail.com>
2012-10-18 16:45:27 -0500

    Update android/src/net/abcdroid/devfest12/io/JSONHandler.java
    se agrego  el  metodo loadResourceJsonRemote(Context context, String url)
    
    y el metodo downloadUrl(String url_)  para traer el  json remotamente.

--- android/src/net/abcdroid/devfest12/io/JSONHandler.java
+++ android/src/net/abcdroid/devfest12/io/JSONHandler.java
@@ -16,9 +16,6 @@
 
 package net.abcdroid.devfest12.io;
 
-import android.content.ContentProviderOperation;
-import android.content.Context;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,38 +23,86 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
+import android.content.ContentProviderOperation;
+import android.content.Context;
+
 /**
- * An abstract object that is in charge of parsing JSON data with a given, known structure, into
- * a list of content provider operations (inserts, updates, etc.) representing the data.
+ * An abstract object that is in charge of parsing JSON data with a given, known
+ * structure, into a list of content provider operations (inserts, updates,
+ * etc.) representing the data.
  */
 public abstract class JSONHandler {
     protected static Context mContext;
 
-    protected JSONHandler(Context context) {
-        mContext = context;
-    }
+	protected JSONHandler(Context context) {
+		mContext = context;
+	}
 
-    public abstract ArrayList<ContentProviderOperation> parse(String json) throws IOException;
+	public abstract ArrayList<ContentProviderOperation> parse(String json) throws IOException;
 
-    /**
-     * Loads the JSON text resource with the given ID and returns the JSON content.
-     */
-    public static String loadResourceJson(Context context, int resource) throws IOException {
-        InputStream is = context.getResources().openRawResource(resource);
-        Writer writer = new StringWriter();
-        char[] buffer = new char[1024];
-        try {
-            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            int n;
-            while ((n = reader.read(buffer)) != -1) {
-                writer.write(buffer, 0, n);
-            }
-        } finally {
-            is.close();
-        }
+	/**
+	 * Loads the JSON text resource with the given ID and returns the JSON
+	 * content.
+	 */
+	public static String loadResourceJson(Context context, int resource) throws IOException {
+		InputStream is = context.getResources().openRawResource(resource);
+		Writer writer = new StringWriter();
+		char[] buffer = new char[1024];
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			int n;
+			while ((n = reader.read(buffer)) != -1) {
+				writer.write(buffer, 0, n);
+			}
+		} finally {
+			is.close();
+		}
 
-        return writer.toString();
-    }
+		return writer.toString();
+	}
+
+	public static String loadResourceJsonRemote(Context context, String url) throws IOException {
+		// InputStream is = context.getResources().openRawResource(resource);
+		InputStream is = downloadUrl(url);
+		Writer writer = new StringWriter();
+		char[] buffer = new char[1024];
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			int n;
+			while ((n = reader.read(buffer)) != -1) {
+				writer.write(buffer, 0, n);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			is.close();
+		}
+
+		return writer.toString();
+	}
+
+	private static InputStream downloadUrl(String url_) {
+		HttpURLConnection con = null;
+		URL url;
+		InputStream is = null;
+		try {
+			url = new URL(url_);
+			con = (HttpURLConnection) url.openConnection();
+			con.setReadTimeout(10000 /* milliseconds */);
+			con.setConnectTimeout(15000 /* milliseconds */);
+			con.setRequestMethod("GET");
+			con.setDoInput(true);			
+			con.connect();
+			is = con.getInputStream();
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
+		return is;
+
+	}
+
 }
